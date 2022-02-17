e75df9038e52680c61dbaed6a240452e4543645a
Kunal Singh <kunalsin9h@gmail.com>
2022-02-16 21:27:34 -0800

    feat: Project init.

---
+++ main.py
@@ -0,0 +1,12 @@
+import time
+from plyer import notification
+
+if __name__ == "__main__":
+    while True:
+        notification.notify(
+            title = "Wash Your Eye!!",
+            message = "Reduce Eye Strain.. Be happy",
+            app_icon = "photo.ico",
+            timeout = 2,
+        )
+        time.sleep(30*60)
